3e8107f1bd484136b151dce2e0c7800c25e1d2ff
rekha thakur <114826952+rekhathakur22@users.noreply.github.com>
2026-05-08 19:48:42 +0000

    succesfully created a BT from preorder sequence

--- trees/binary_tree_impl.cpp
+++ trees/binary_tree_impl.cpp
@@ -1,8 +1,37 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
+class Node{
+public:
+int data;
+Node* left;
+Node* right;
+
+Node(int val)
+{
+    data=val;
+    left=right=NULL;
+}
+};
+
+
+Node* build_tree(vector<int> arr){
+    static int ind = -1;
+   ++ind;
+   if(arr[ind] == -1 || ind == arr.size()) return NULL;
+   Node* node = new Node(arr[ind]);
+   
+   node->left = build_tree(arr);
+   node->right = build_tree(arr);
+   
+   return node;
+ }
 
 int main()
 {
-    cout<<"hello hyy";
+    vector<int> arr = {1,3,-1,-1,4,5,-1,-1,6,-1,-1};
+    Node* root = build_tree(arr);
+    cout<<root->data<<endl;
+    
     return 0;
 }
