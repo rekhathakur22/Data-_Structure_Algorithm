72b951dc2b72b1376c0db86268a539d04ca4a584
rekha thakur <114826952+rekhathakur22@users.noreply.github.com>
2026-05-11 14:10:53 +0000

    preorder traversal done

--- trees/binary_tree_impl.cpp
+++ trees/binary_tree_impl.cpp
@@ -2,44 +2,42 @@
 using namespace std;
 
 class Node{
-public:
-int data;
-Node* left;
-Node* right;
-
-Node(int val)
-{
-    data=val;
-    left=right=NULL;
-}
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val)
+    {
+        data = val;
+        left=right=NULL;
+    }
+    
 };
 
-
+static int ind=-1;
 Node* build_tree(vector<int> arr){
-    static int ind = -1;
-   ++ind;
-   if(arr[ind] == -1 || ind == arr.size()) return NULL;
-   Node* node = new Node(arr[ind]);
-   
-   node->left = build_tree(arr);
-   node->right = build_tree(arr);
-   
-   return node;
- }
+    ind++;
+    if(ind >= arr.size() || arr[ind] == -1) return NULL;
+    Node* node = new Node(arr[ind]);
+    node->left = build_tree(arr);
+    node->right = build_tree(arr);
+    return node;
+}
 
- void inorder(Node* root)
- {
-     if(root == NULL) return;
-     inorder(root->left);
-     cout<<root->data<<" ";
-     inorder(root->right);
- }
+void preorderer_trav(Node* root)
+{
+    if(root == NULL) return ;
+    cout<<root->data<<endl;
+    preorderer_trav(root->left);
+    preorderer_trav(root->right);
+}
+
 
 int main()
 {
-    vector<int> arr = {1,3,-1,-1,4,5,-1,-1,6,-1,-1};
-    Node* root = build_tree(arr);
-    cout<<root->data<<endl;
-    
+    vector<int> arr = {1,3,-1,-1,4,5,-1,-1,6};
+    Node* root =  build_tree(arr);
+    preorderer_trav(root);
+   
     return 0;
 }
